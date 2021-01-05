79a7f45bbd63ae7a59583815305c2ca5ec5d4b88
gautamsontu <32488648+gautamsontu@users.noreply.github.com>
2021-01-05 12:59:13 +0530

    Add files via upload

---
+++ Header Files/UART.h
@@ -0,0 +1,8 @@
+#ifndef _UART_h_
+#define _UART_h_
+
+Config_UART();
+void Set_UART_Data(unsigned char);
+unsigned char Get_UART_data();
+	
+#endif
---
+++ Header Files/lcd.h
@@ -0,0 +1,10 @@
+#ifndef _lcd_h_
+#define _lcd_h_
+
+void Set_LCD_Command(unsigned char);
+void Set_LCD_Data(unsigned char);
+void Initialize_LCD();
+void Set_LCD_String(unsigned char);
+
+
+#endif
